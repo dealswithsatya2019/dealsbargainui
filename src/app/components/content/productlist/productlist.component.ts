8ef7d9828bfe5113006249747292db7fc3d8c9c6
dealswithsatya2019 <dealswithsatya2019@gmail.com>
2019-10-27 11:19:57 +0530

    product list page left pannel made dynamic based on api respone. filtering the data based on brand click and price range.

--- src/app/components/content/productlist/productlist.component.ts
+++ src/app/components/content/productlist/productlist.component.ts
@@ -12,110 +12,65 @@ import { searchreponse } from 'src/app/models/searchResponse';
 })
 export class ProductlistComponent implements OnInit {
 
-  constructor(private _Activatedroute: ActivatedRoute, public _productservice: ProductService,public _router: Router,public dialog: MatDialog) { }
+  constructor(private _Activatedroute: ActivatedRoute, public _productservice: ProductService, public _router: Router, public dialog: MatDialog) { }
   cname: any;
   scname: any;
   sub;
   products: any[];
+  distinctbrandsArray: Array<string>;
+  selectedBrandsArray: Array<string> = [];
+  fromPrice: number = 0;
+  toPrice: number = 0;
+
   ngOnInit() {
     this.sub = this._Activatedroute.paramMap.subscribe(params => {
-      console.log('params'+params);
+      console.log('params' + params);
       this.cname = params.get('cname');
       this.scname = params.get('scname');
       this._productservice.getProductlist(this.cname, this.scname, 'us', 0, 20).subscribe(
-        /*data => {
-          const arr = [];
-          data.responseObjects.forEach((productDet) => {
-            const obj = {};
-            obj['name'] = productDet.item_name;
-            obj['image_uri'] = productDet.image;
-            obj['actual_price'] = productDet.custom_price;
-            obj['offer_price'] = productDet.prepay_price;
-            
-            arr.push(obj);
-
-          });
-          this.products = arr;*/
-          (results: searchreponse) => {
-            this.products=results.responseObjects;
+        (results: searchreponse) => {
+          this.products = results.responseObjects;
+          this.getDistinctBrands();
         });
     });
-
   }
 
   showProductDetails(params) {
     this._productservice.routeProductDetails(params);
   }
 
-  /*public products = [
-    {
-      name: "Bata, Reebok, Nike and popular brand shoes",
-      image_uri: '../../../assets/img/1.jpeg',
-      offer_per:'50%'
-    },
-    {
-      name: "Watch",
-      image_uri: '../../../assets/img/2.jpeg',
-      offer_per:'20%'
-    },
-    {
-      name: "T-Shirt",
-      image_uri: '../../../assets/img/9.jpeg'
-    },
-    {
-      name: "Bluetooth Headset",
-      image_uri: '../../../assets/img/4.jpeg'
-    },
-    {
-      name: "Speakers",
-      image_uri: '../../../assets/img/5.jpeg'
-    },
-    {
-      name: "Awesome deals on luggage bags",
-      image_uri: '../../../assets/img/3.jpeg'
-    },
-    {
-      name: "Huge discounts on mobiles",
-      image_uri: '../../../assets/img/6.jpeg'
-    },
-    {
-      name: "Huge discounts on mobiles",
-      image_uri: '../../../assets/img/8.jpeg'
-    },
-    {
-      name: "Huge discounts on mobiles",
-      image_uri: '../../../assets/img/10.jpeg'
-    },
-    {
-      name: "Speakers",
-      image_uri: '../../../assets/img/11.jpeg'
-    },
-    {
-      name: "Huge discounts on mobiles",
-      image_uri: '../../../assets/img/8.jpeg'
-    },
-    {
-      name: "Huge discounts on mobiles",
-      image_uri: '../../../assets/img/10.jpeg'
-    },
-    {
-      name: "Speakers",
-      image_uri: '../../../assets/img/11.jpeg'
+  getDistinctBrands() {
+    this.distinctbrandsArray = this.products.map(item => item.brand_name).filter((value, index, self) => self.indexOf(value) === index)
+  }
+
+  refreshBrands(event: any, brand_name) {
+    if (event.checked) {
+      if (!this.selectedBrandsArray.includes(brand_name)) {
+        this.selectedBrandsArray.push(brand_name);
+      }
+    } else {
+      if (this.selectedBrandsArray.includes(brand_name)) {
+        this.selectedBrandsArray = this.selectedBrandsArray.filter(item => item !== brand_name);
+      }
     }
+  }
 
-  ];
+  updateFromPrice(event) {
+    this.fromPrice = event.value;
+  }
+  updateToPrice(event) {
+    this.toPrice = event.value;
+  }
 
-*/
-
-openShare(event : any) {
-  const dialogConfig = new MatDialogConfig();
-  dialogConfig.closeOnNavigation = true;
-  dialogConfig.autoFocus = true;
-  dialogConfig.width = '200px';
-  dialogConfig.height = '150px';
-  let x = event.x-50;
-  let y = event.y-50;
-  dialogConfig.position = {left:x+'px',top:y+"px"};
-  this.dialog.open(SocialshareComponent, dialogConfig);
-}
+  openShare(event: any) {
+    const dialogConfig = new MatDialogConfig();
+    dialogConfig.closeOnNavigation = true;
+    dialogConfig.autoFocus = true;
+    dialogConfig.width = '200px';
+    dialogConfig.height = '150px';
+    let x = event.x - 50;
+    let y = event.y - 50;
+    dialogConfig.position = { left: x + 'px', top: y + "px" };
+    this.dialog.open(SocialshareComponent, dialogConfig);
+  }
 }
