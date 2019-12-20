436ee9154c17bd2baa2d112107fbc9471899eea0
dealswithsatya2019 <veerendragreat@gmail.com>
2019-12-21 00:12:23 +0530

    -> Added model class

---
+++ src/app/models/OrderItemInfo.ts
@@ -0,0 +1,9 @@
+import { ProductDetails } from 'src/app/models/ProductDetails';
+
+
+export class OrderItemInfo extends ProductDetails {
+    show_cancel_button: string = 'no';
+    show_return_button: string = 'no';
+
+
+}
