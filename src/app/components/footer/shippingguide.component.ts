b7356c87168517f6e4c616bcb2b82e6eafbd7263
dealswithsatya2019 <dealswithsatya2019@gmail.com>
2020-01-12 18:29:10 +0530

    contact us changes. Added file option to upload attachment.

--- src/app/components/footer/shippingguide.component.ts
+++ src/app/components/footer/shippingguide.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, ViewChild, ElementRef } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { Subscription } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
+import { CartService } from 'src/app/services/cart.service';
 
 @Component({
   selector: 'app-shippingguide',
@@ -7,9 +13,82 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ShippingguideComponent implements OnInit {
 
-  constructor() { }
+  subscriptions = new Subscription();
+  private APIEndpoint: string = environment.APIEndpoint;
+  constructor(public http: HttpClient,private cartService: CartService){
 
-  ngOnInit() {
   }
+  ngOnInit(): void {
+    throw new Error("Method not implemented.");
+  }
+
+  saveContact: FormGroup = new FormGroup({
+    filename: new FormControl(null),
+    contact: new FormControl(""),
+    subject: new FormControl(""),
+    ordernumber: new FormControl(""),
+    trackingnumber: new FormControl(""),
+    description: new FormControl(""),
+    quantity: new FormControl(""),
+    filecontent: new FormControl(""),
+  });
+
+  fileContent: any;
+  fileSelect(event) {
+    this.fileContent = event.target.files[0];
+    this.fileToBase64(event, (result: any, headers: any) => {
+      this.fileContent = result;
+      console.log("file ",this.fileContent);
+    });
+  }
+
+  public fileToBase64(event, callback) {
+    let url: any;
+    let header: any;
+    if (event.target.files && event.target.files[0]) {
+      const srcFile = event.target.files[0];
+      const reader = new FileReader();
+      const textReader = new FileReader();
+      reader.readAsDataURL(srcFile); // read file as data url
+      reader.onload = (event: any) => { // called once readAsDataURL is completed
+        url = (<FileReader>event.target).result;
+        const index = url.indexOf(',') + 1;
+        url = url.slice(index);
+        if (typeof callback === 'function') {
+        }
+      };
+      textReader.onload = function (e) {
+        header = textReader.result;
+        header = header.split('\n');
+        header = header[0].split('|');
+        callback(url, header);
+      };
+      textReader.readAsText(srcFile);
+    }
+  }
+
+  funSave() {
+    this.saveContact.controls.filecontent.setValue(this.fileContent);
+    let contactReqInfo = JSON.parse(JSON.stringify(this.saveContact.value));
+    console.log("form dataa ",contactReqInfo);
+    this.saveContactusReq(contactReqInfo);
+  }
+
+  public saveContactusReq(contactReqInfo: string) {
+    this.subscriptions.add(this.http.post(this.APIEndpoint + "/user/contactus", contactReqInfo,
+      { headers: { 'Content-Type': 'application/json'} }).subscribe(data => {
+        let jsonobj = JSON.parse(JSON.stringify(data));
+        if (jsonobj.statusCode == 200) {
+          this.cartService.raiseAlert("The request has been raised successfully.");
+        } else {
+          this.cartService.raiseAlert("Failed to creat the request, Please send a mail.");
+        }
+      }));
+  }    
+
+  ngOnDestroy(): void {
+    this.subscriptions.unsubscribe();
+  }
+
 
 }
